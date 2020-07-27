da73c19e913436714ad8bf1da6017cec66c7444e
Harshvardhan <harshvardhanvipat@gmail.com>
2020-07-27 18:11:25 +1000

    Updates

---
+++ precache-manifest.4e1d46490593f3f279cd30324977eca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24cbcbc8cddf004499dfe181e957480",
+    "url": "/monster-react-app/index.html"
+  },
+  {
+    "revision": "814785a0ef4b38e316dd",
+    "url": "/monster-react-app/static/css/main.eff05799.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-react-app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-react-app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814785a0ef4b38e316dd",
+    "url": "/monster-react-app/static/js/main.2bcbbb8a.chunk.js"
+  },
+  {
+    "revision": "d3b431bf97b15e306ffc",
+    "url": "/monster-react-app/static/js/runtime-main.dc588f57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
